8dbc234bdf395dbb090dc9d13db9d2a83e3e3ff1
andrej.lavrinovic <andrej.lavrinovic@smartbear.com>
2019-04-20 14:36:08 +0100

    initialization

---
+++ src/main/java/ie/altech/schoolethon/schoolethonlimits/SchoolethonLimitsApplication.java
@@ -0,0 +1,13 @@
+package ie.altech.schoolethon.schoolethonlimits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolethonLimitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolethonLimitsApplication.class, args);
+	}
+
+}
